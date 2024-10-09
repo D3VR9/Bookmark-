498fb807505dc77fb900704bcd431db5e80b0d38
D3VR9 <sonakh4n@gmail.com>
2024-10-09 18:03:48 +0530

    Update Bookmark.py

--- Bookmark.py
+++ Bookmark.py
@@ -1,8 +1,5 @@
 if __name__ == "__main__":
     import Bookmark
-    # Replace with any function that exists in Bookmark
-    # For example, if there is a `run` function:
-    if hasattr(Bookmark, 'run'):
-        Bookmark.run()
-    else:
-        print("No function to run in Bookmark module.")
+
+    # List all attributes and methods of the Bookmark module
+    print(dir(Bookmark))
